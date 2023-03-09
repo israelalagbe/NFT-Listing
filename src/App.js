aecd1e7dc5b26283bc1241d6a6e326f24f719879
Israel Alagbe <israelalagbe53@gmail.com>
2023-03-09 10:52:31 +0100

    Added nft design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Modal from './components/Modal';
 
-function App() {
+// import { getNFTs } from './api'; // function to retrieve NFTs from API
+
+function NFTGrid({ address }) {
+  const [nfts, setNFTs] = useState([]);
+  const [selectedNFT, setSelectedNFT] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      // const nfts = await getNFTs(address);
+      const nfts = [];
+      for(let i=0;i<10;i++) {
+        nfts.push({
+          id: i,
+          name: "Staff Name",
+          image: "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+          description: "Start editing to see some magic happen :)"
+        });
+      }
+      console.log(nfts)
+      setNFTs(nfts);
+    }
+    fetchData();
+  }, [address]);
+
+  const handleCardClick = (nft) => {
+    setSelectedNFT(nft);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedNFT(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nft-grid">
+      {nfts.map((nft) => (
+        <div className="nft-card" key={nft.id} onClick={() => handleCardClick(nft)}>
+          <img src={nft.image} alt={nft.name} />
+          <h3>{nft.name}</h3>
+          <p>{nft.description}</p>
+        </div>
+      ))}
+      {selectedNFT && (
+        <Modal item={selectedNFT} handleCloseModal={handleCloseModal} />
+      )}
     </div>
   );
 }
 
-export default App;
+
+export default function App() {
+  return (
+    <NFTGrid address="abala" />
+  );
+}
---
+++ src/components/Modal/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './index.scss';
+
+/**
+ *
+ * @param {Object} props
+ * @param {{
+ * name: string,
+ * image: string,
+ * description: string,
+ * permalink: string,
+ * }} props.item
+ * @param {Function} props.handleCloseModal
+ * @returns
+ */
+function Modal({ item, handleCloseModal }) {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={handleCloseModal}>
+          &times;
+        </span>
+        <h2>{item.name}</h2>
+        <img src={item.image} alt={item.name} />
+        <p>{item.description}</p>
+        <a href={item.permalink} target="_blank" rel="noreferrer">
+          Purchase on OpenSea
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
